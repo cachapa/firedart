f146efe6f713c1bcf6efae040ef309cbedd1c79a
Demoniaque <saadodi44@gmail.com>
2020-05-14 21:06:17 +0300

    simplify docs, add trailing line in .gitignore, make serviceAccountString final

--- example/admin.dart
+++ example/admin.dart
@@ -1,33 +1,16 @@
 import 'package:firedart/firedart.dart';
 
-/// Demonstrates the Firestore Admin API using a privileged service account.
-/// Operations bypass all Firestore database rules that apply to regular users.
-///
-/// Usage:
-/// Create a service account with permission to access Firestore.
-/// See the console:
-/// https://console.firebase.google.com/u/0/project/_/settings/serviceaccounts/adminsdk
-/// Download and save the service account json file.
-/// ****** DO NOT CHECK THIS FILE INTO git! *****
-/// It is highly privileged and should be kept secret.
-///
-/// If you want to pass the file via environment variables:
-///   Set the environment variable GOOGLE_APPLICATION_CREDENTIALS to the
-///   path to this file.  For example:
-///   GOOGLE_APPLICATION_CREDENTIALS=./service-account.json
-///   Then you can use ServiceAccount.fromEnvironmentVariable();
-///   You may specify your own environment variable key optionally if you'd like.
-///
-/// If you want to pass the file via direct json string:
-///   Copy the service account json and use ServiceAccount.fromJson(String);
-///   Again, DO NOT PUSH THE FILE HOLDING THIS DATA TO A PUBLIC REPOSITORY.
-
 const apiKey = 'Project Settings -> General -> Web API Key';
 const projectId = 'Project Settings -> General -> Project ID';
 
+/// Note: Beware of pushing your credentials to source control.
 Future main() async {
-  FirebaseAuth.initialize(apiKey, VolatileStore(),
-      serviceAccount: ServiceAccount.fromJson(r'''
+  /// You can use environment variable look-up to find your service-account.json like so:
+  /// (GOOGLE_APPLICATION_CREDENTIALS is the default environment variable we look for.)
+  /// FirebaseAuth.initialize(apiKey, VolatileStore(), serviceAccount: ServiceAccount.fromEnvironmentVariable());
+
+  /// Alternatively:
+  FirebaseAuth.initialize(apiKey, VolatileStore(), serviceAccount: ServiceAccount.fromJson(r'''
       <Project Settings -> Service Accounts -> generate new private key>
       Paste the json here
     '''));
@@ -46,8 +29,7 @@ Future main() async {
   var document = await ref.get();
   print('snapshot: ${document['value']}');
 
-  print(
-      'Sleeping for 30 seconds. You can make changes to test/doc in the UI console');
+  print('Sleeping for 30 seconds. You can make changes to test/doc in the UI console');
   await Future.delayed((const Duration(seconds: 30)));
   print('closing the connection');
   await Firestore.instance.close();
--- lib/auth/serviceaccount/service_account.dart
+++ lib/auth/serviceaccount/service_account.dart
@@ -1,15 +1,12 @@
 import 'package:firedart/auth/serviceaccount/access_exporter.dart';
 
 class ServiceAccount {
-  String serviceAccountString;
+  final String serviceAccountString;
 
   ServiceAccount.fromJson(this.serviceAccountString);
 
-  ServiceAccount.fromEnvironmentVariable(
-      {String environmentVariable = 'GOOGLE_APPLICATION_CREDENTIALS'}) {
-    serviceAccountString =
-        getIOAccess().getEnvironmentVariable(environmentVariable);
-  }
+  ServiceAccount.fromEnvironmentVariable({String environmentVariable = 'GOOGLE_APPLICATION_CREDENTIALS'})
+      : serviceAccountString = getIOAccess().getEnvironmentVariable(environmentVariable);
 }
 
 abstract class AbstractPlatformAccess {
