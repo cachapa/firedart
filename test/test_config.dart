5bf5abcfb1390301fd0e99c1b39a9ef44842bfe4
Daniel Cachapa <cachapa@gmail.com>
2023-04-11 12:40:57 +0100

    Rename test config file

---
+++ test/test_config.dart
@@ -0,0 +1,4 @@
+const apiKey = 'Project Settings -> General -> Web API Key';
+const projectId = 'Project Settings -> General -> Project ID';
+const email = 'you@server.com';
+const password = '1234';
