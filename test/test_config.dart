b7f6f87a3b9d7dac8fa76e340247f1436720fb4d
Daniel Cachapa <cachapa@gmail.com>
2023-06-12 14:10:58 +0200

    Update test_config.dart

--- test/test_config.dart
+++ test/test_config.dart
@@ -2,4 +2,3 @@ const apiKey = 'Project Settings -> General -> Web API Key';
 const projectId = 'Project Settings -> General -> Project ID';
 const email = 'you@server.com';
 const password = '1234';
-const customToken = 'see https://firebase.google.com/docs/auth/admin/create-custom-tokens';
